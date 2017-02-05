08aa0d67b394aa4ebb3dad6a8c614ed7099d2620
obozhdi <obozhdi@obozhdi.com>
2017-02-06 00:51:02 +0200

    add keyboard appearance logic

--- TwitterTestTask/LoginViewController.swift
+++ TwitterTestTask/LoginViewController.swift
@@ -9,27 +9,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var twitterNameField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.twitterNameField.resignFirstResponder()
+        
+        return true
     }
-    */
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func getTweets(_ sender: Any) {
+        
+    }
+    
+//    -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+//    {
+//    [self.view endEditing:YES];
+//    }
 }
