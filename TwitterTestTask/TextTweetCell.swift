dc4e9c02edb127ec2d9095514fa6e7126a9470fd
obozhdi <obozhdi@obozhdi.com>
2017-02-06 01:45:46 +0200

    assemble table, create custom cells, attach cells

---
+++ TwitterTestTask/TextTweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextTweetCell.swift
+//  TwitterTestTask
+//
+//  Created by obozhdi on 06/02/2017.
+//  Copyright © 2017 obozhdi. All rights reserved.
+//
+
+import UIKit
+
+class TextTweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
