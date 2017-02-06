188700e627be512876d55c64dfabea9eb7fc9399
obozhdi <obozhdi@obozhdi.com>
2017-02-07 00:29:12 +0200

    add swifter, add twitter keys

--- TwitterTestTask/LoginViewController.swift
+++ TwitterTestTask/LoginViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import SwifteriOS
+import Accounts
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var twitterNameField: UITextField!
+    
+    var swifter: Swifter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.swifter = Swifter(consumerKey: "nxJuCvaI8TgusEMBEWkMza3i1", consumerSecret: "nLStckpxQjeAqb2AB7EKF1Hye7RM7M5AGUgdbiBUgvkbAR0dqC")
+
+        let url = URL(string: "tweettestapp://success")!
+        
+        swifter?.authorize(with: url, presentFrom: self, success: { _ in
+            print("good")
+        }, failure: nil)
+        
+        swifter?.getHomeTimeline(count: 50, success: { json in
+            print(json)
+        }, failure: { error in
+            print(error)
+        })
 
     }
     
